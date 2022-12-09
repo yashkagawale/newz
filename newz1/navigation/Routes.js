3a2ec2759ee9060eec3afe1e5766bcfe4f613aa7
Yash Kagawale <kagawaleyash@gmail.com>
2022-12-09 18:53:02 +0530

    Second Commit

--- newz1/navigation/Routes.js
+++ newz1/navigation/Routes.js
@@ -1,43 +1,42 @@
 import React, { useContext, useState, useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
-// import {AuthContext} from './AuthProvider'
-// import auth from '@react-native-firebase/auth' 
-
+import {AuthContext} from './AuthProvider'
+import auth from '@react-native-firebase/auth'
 
 import AuthStack from "./AuthStack";
-// import AppStack from './AppStack';
-
-// const Routes = () => {
-
-//     const {user, setUser} = useContext(AuthContext);
-//     const [intializing, setInitializing] = useState(true);
-
-//     const onAuthStateChange = (user) => {
-//         setUser(user);
-//         if(intializing) setInitializing(false);
-//     }
-
-//     useEffect (() => {
-//         const subscriber = auth().onAuthStateChange(onAuthStateChange);
-//         return subscriber;
-//     }, []);
-
-//     if(intializing) return null;
-
-//     return (
-//         <NavigationContainer>
-//             {user ? <AppStack/> : <AuthStack />}
-//         </NavigationContainer>
-//     );
-// };
-
+import AppStack from './AppStack';
 
 const Routes = () => {
+
+    const {user, setUser} = useContext(AuthContext);
+    const [intializing, setInitializing] = useState(true);
+
+    const onAuthStateChange = (user) => {
+        setUser(user);
+        if(intializing) setInitializing(false);
+    }
+
+    useEffect (() => {
+        const subscriber = auth().onAuthStateChange(onAuthStateChange);
+        return subscriber;
+    }, []);
+
+    if(intializing) return null;
+
     return (
         <NavigationContainer>
-            <AuthStack/>
+            {user ? <AppStack/> : <AuthStack />}
         </NavigationContainer>
     );
 };
+
+
+// const Routes = () => {
+//     return (
+//         <NavigationContainer>
+//             <AuthStack/>
+//         </NavigationContainer>
+//     );
+// };
 
 export default Routes;
